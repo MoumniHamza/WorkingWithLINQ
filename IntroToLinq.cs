2d5c54adba1ba7fae0ebed3b62189407abcb4d5f
Hamza Moumni <94747@supinfo.com>
2015-10-02 18:15:27 -0700

    Working with queries using LINQ

---
+++ IntroToLinq.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[7] { 1, 2, 3, 4, 5, 6, 7 };
+            var results = from item in numbers
+                          where (item % 2) == 0
+                          select item;
+            int evennumber = results.Count();
+            Console.WriteLine(evennumber);
+            Console.ReadLine();
+
+          
+
+        }
+    }
+}
---
+++ Querybycity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+
+
+
+    class DataTransformations
+    {
+        static void Main()
+        {
+            string sentence = "Hello my name is hamza and I am 24";
+            string[] words = sentence.Split(' ');
+
+            var result = from w in words
+                         group w.ToUpper() by w.Length into gr
+                         orderby gr.Key
+                         select new { length = gr.Key, words = gr };
+            foreach(var results in result)
+            {
+                Console.WriteLine("Words of length {0} : ", results.length);
+                     foreach(string finalresults in results.words)
+                                  Console.WriteLine(finalresults);
+              
+
+            }
+
+            Console.WriteLine("press enter to exit");
+            Console.ReadLine();
+                         
+        }
+
+    }
+
+}
+
